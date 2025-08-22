9608b5b812e7a2364eb210fb0caa29c6f8295adc
ParsaAnsariAminjan <parsaansari100@gmail.com>
2025-08-23 01:12:14 +0330

    Add Partial Head & Footer

---
+++ EShope/ViewComponents/SiteFooterViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace EShope.ViewComponents
+{
+    public class SiteFooterViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("SiteFooter");
+        }
+    }
+}
